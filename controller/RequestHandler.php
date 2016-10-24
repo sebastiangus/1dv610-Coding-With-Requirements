d5b93e0399b92157e016e645efb81e3f717146cc
sebastiangus <sg222pm@student.lnu.se>
2016-10-24 08:40:32 +0200

    SessionTracker and RequestHandler added to controll login/logout state

---
+++ controller/RequestHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastiangustavsson
+ * Date: 2016-10-24
+ * Time: 07:56
+ */
+
+namespace controller;
+
+
+class RequestHandler
+{
+
+}
---
+++ model/SessionTracker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastiangustavsson
+ * Date: 2016-10-23
+ * Time: 14:33
+ */
+
+namespace model;
+
+
+class SessionTracker
+{
+
+}
